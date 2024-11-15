e1a32b29b49a84c603bf15ce18d1af887ac3a0e0
Olivier <olefebvre21@icloud.com>
2024-11-15 13:18:49 -0500

    Adding NGINX Http Script File

---
+++ pelican/nginx/nginx_http_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Nginx
+sudo apt update
+sudo apt install -y nginx
+echo "Nginx installed successfully."
+
+# Remove the default Nginx configuration file if it exists
+rm -f /etc/nginx/sites-enabled/default
+echo "Default Nginx configuration removed."
+
+# Download the pelican.conf file and place it in /etc/nginx/sites-available
+curl -o /etc/nginx/sites-available/pelican.conf https://raw.githubusercontent.com/GrumpyOli/scripts/refs/heads/main/pelican/nginx/pelican_http.conf
+echo "pelican.conf downloaded successfully."
+
+# Create a symbolic link in /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/pelican.conf /etc/nginx/sites-enabled/pelican.conf
+echo "Symbolic link created successfully."
+
+# Enable the Nginx service
+sudo systemctl enable nginx
+echo "Nginx service enabled successfully."
+
+# Start the Nginx service
+sudo systemctl restart nginx
+echo "Nginx service started successfully."
+
+echo "Script completed successfully."
