f238cfce13c1fc958670378040d57a3b5c815231
Olivier <olefebvre21@icloud.com>
2024-11-15 13:26:57 -0500

    Update nginx_http_install.sh

--- pelican/nginx/nginx_http_install.sh
+++ pelican/nginx/nginx_http_install.sh
@@ -6,11 +6,11 @@ sudo apt install -y nginx
 echo "Nginx installed successfully."
 
 # Remove the default Nginx configuration file if it exists
-rm -f /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/default
 echo "Default Nginx configuration removed."
 
-rm -f /etc/nginx/sites-enabled/pelican.conf
-rm -f /etc/nginx/sites-available/pelican.conf
+sudo rm -f /etc/nginx/sites-enabled/pelican.conf
+sudo rm -f /etc/nginx/sites-available/pelican.conf
 echo "Old Pelican configuration removed."
 
 # Download the pelican.conf file and place it in /etc/nginx/sites-available
