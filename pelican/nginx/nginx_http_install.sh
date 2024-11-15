de71dbf7d4c6779c6444ed8ef488154947312103
Olivier <olefebvre21@icloud.com>
2024-11-15 13:34:15 -0500

    Update nginx_http_install.sh

--- pelican/nginx/nginx_http_install.sh
+++ pelican/nginx/nginx_http_install.sh
@@ -9,17 +9,20 @@ echo "Nginx installed successfully."
 sudo rm -f /etc/nginx/sites-enabled/default
 echo "Default Nginx configuration removed."
 
-sudo rm -f /etc/nginx/sites-enabled/pelican.conf
-sudo rm -f /etc/nginx/sites-available/pelican.conf
-echo "Old Pelican configuration removed."
+# Check if pelican.conf already exists in /etc/nginx/sites-available
+if [ ! -f /etc/nginx/sites-available/pelican.conf ]; then
 
-# Download the pelican.conf file and place it in /etc/nginx/sites-available
-curl -o /etc/nginx/sites-available/pelican.conf https://raw.githubusercontent.com/GrumpyOli/scripts/refs/heads/main/pelican/nginx/pelican_http.conf
-echo "pelican.conf downloaded successfully."
+    # Download the pelican.conf file and place it in /etc/nginx/sites-available
+    curl -o /etc/nginx/sites-available/pelican.conf https://raw.githubusercontent.com/GrumpyOli/scripts/refs/heads/main/pelican/nginx/pelican_http.conf
+    echo "pelican.conf downloaded successfully."
 
-# Create a symbolic link in /etc/nginx/sites-enabled
-sudo ln -s /etc/nginx/sites-available/pelican.conf /etc/nginx/sites-enabled/pelican.conf
-echo "Symbolic link created successfully."
+    # Create a symbolic link in /etc/nginx/sites-enabled
+    sudo ln -s /etc/nginx/sites-available/pelican.conf /etc/nginx/sites-enabled/pelican.conf
+    echo "Symbolic link created successfully."
+
+else
+    echo "pelican.conf already exists. Skipping download."
+fi
 
 # Enable the Nginx service
 sudo systemctl enable nginx
@@ -28,5 +31,11 @@ echo "Nginx service enabled successfully."
 # Start the Nginx service
 sudo systemctl restart nginx
 echo "Nginx service started successfully."
+
+cd /var/www/pelican
+sudo chmod -R 755 storage/* bootstrap/cache/
+sudo chown -R www-data:www-data /var/www/pelican
+
+sudo php artisan p:environment:setup
 
 echo "Script completed successfully."
